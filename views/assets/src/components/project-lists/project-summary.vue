f0f2dc9763d8b69a84d4ec95b307b3f736efe222
Mahbub Rabbani <mahbub.rucse@gmail.com>
2023-11-08 20:01:45 +0600

    Fix search box position

--- views/assets/src/components/project-lists/project-summary.vue
+++ views/assets/src/components/project-lists/project-summary.vue
@@ -123,11 +123,25 @@
                         </ul>
 
                         <!-- progress -->
-                        <div class="pm-project-progress">
-                            <!-- <div :style="projectCompleteStatus(project)" class="bar completed"></div> -->
-                            <span :style="projectCompleteStatus(project)" class="pm-project-status"></span>
-                        </div>
+                        
+                        <!-- <div class="pm-project-progress-wrap"> -->
 
+                        <div class="pm-project-progress-wrapper">
+                            <!-- <div class="pm-project-progress">
+                                <span :style="projectCompleteStatus(project)" class="pm-project-status"></span>
+                            </div>
+                             -->
+                            <div class="progress-bar">
+                                <div class="pm-project-progress">
+                                    <span :style="projectCompleteStatus(project)" class="pm-project-status"></span>
+                                </div>
+                            </div>
+                            <div class="progress-stats">
+                                <div class="pm-project-status-stats">
+                                    <span>{{ project.meta.data.total_complete_tasks }}</span>/<span>{{ project.meta.data.total_tasks }}</span>
+                                </div>
+                            </div>
+                        </div>
                         <!-- project users -->
                         <div class="pm-project-users">
                             <ul class="pm-list-inline">
@@ -165,239 +179,219 @@
 
 
 <script>
+import Favourite from "./favourite.vue";
+import Mixins from "./mixin";
 
-    import Favourite from './favourite.vue';
-    import Mixins from './mixin';
-
-    export default {
-        mixins: [Mixins],
-        data () {
-            return {
-                is_active_settings: false,
-                is_update: false,
-                project: {},
-                project_action: __( 'Project Actions', 'wedevs-project-manager' ),
-                dropdownToggleStatus: false,
-                tooltipLabels: {
-                    tasks: __( ' Tasks', 'wedevs-project-manager' ),
-                    discussions: __( ' Discussions', 'wedevs-project-manager' ),
-                    tasksLists: __( ' Task Lists', 'wedevs-project-manager' ),
-                    files: __( ' Files', 'wedevs-project-manager' ),
-                    milestones: __( ' Milestones', 'wedevs-project-manager' ),
-                    comments: __( ' Comments', 'wedevs-project-manager' ),
-                }
-            }
-        },
-        computed: {
-            projects () {
-                this.$root.$store.state.projects.forEach(function(project) {
-                    pm.Vue.set(project, 'showMoreUser', false );
-                    pm.Vue.set(project, 'showDropDownMenu', false );
-                });
-                return this.$root.$store.state.projects;
-            },
-            meta () {
-                return this.$root.$store.state.projects_meta;
-            },
-        },
-
-        components: {
-            Favourite
-        },
-
-        created () {
-            window.addEventListener('click', this.windowActivity);
-        },
-
-        methods: {
-            windowActivity (el) {
-                var element = jQuery(el.target),
-                    moreUser = element.closest('.pm-more-users'),
-                    projectId = element.data('project_id'),
-                    moreMenu = element.closest('.pm-has-dropdown'),
-                    menuProjectId = element.data('project_id');
-
-                if(!moreUser.length) {
-                    this.projects.forEach(function(project) {
-                        project.showMoreUser = false;
-                    });
-                } else {
-                    this.projects.forEach(function(project) {
-                        if(project.id != projectId) {
-                            project.showMoreUser = false;
-                        }
-                    });
-                }
-
-                if(!moreMenu.length) {
-                    this.projects.forEach(function(project) {
-                        project.showDropDownMenu = false;
-                    });
-                } else {
-                    this.projects.forEach(function(project) {
-                        if(project.id != menuProjectId) {
-                            project.showDropDownMenu = false;
-                        }
-                    });
-                }
-                
-            },
-            showMoreUser (project) {
-                project.showMoreUser = project.showMoreUser ? false : true;
-            },
-
-            pmDropDownOpen (project) {
-                if(project.showMoreUser) {
-                    return 'pm-dropdown-open';
-                }
-
-                return '';
-            },
-
-            // dropdown trigger
-            dropdownTrigger (project) {
-                project.showDropDownMenu = project.showDropDownMenu ? false : true;
-            },
-
-            // dropdown class toggler
-            dropdownToggleClass(project) {
-                if(project.showDropDownMenu){
-                    return "pm-settings pm-dropdown-menu pm-dropdown-open";
-                } else {
-                    return "pm-settings pm-dropdown-menu";
-                }
-            },
-
-            settingsShowHide (project) {
-                project.settings_hide = project.settings_hide ? false : true;
-            },
-
-            projectCompleteStatus (project) {
-                var progress = 0;
-                if (typeof project.meta !== 'undefined') {
-                    var total_task = parseInt(project.meta.data.total_tasks, 10) || 0;
-                    var completed = parseInt(project.meta.data.total_complete_tasks, 10) || 0;
-                    progress = ((100 * completed) /  total_task);
-                }
-                return {
-                    width: progress + '%' 
-                };
-            },
-            projectMarkAsDoneUndone (project) {
-                var self = this;
-                var status = '';
-
-                if(
-                    project.status=='complete'
-                        ||
-                    project.status=='1'
-                ) {
-                    status = 'incomplete';
-                }
-
-                if(
-                    project.status=='incomplete'
-                        ||
-                    project.status=='0'
-                ) {
-                    status = 'complete';
-                }
-           
-                var args ={
-                    data:{
-                        'id': project.id,
-                        'title': project.title,
-                        'status': status,
-                    },
-                    callback: function(project) {
-
-                        switch (project.data.status) {
-                            
-                            case 'complete':
-                                self.getProjects({
-                                    conditions:{
-                                        status: 'incomplete',
-                                        with: 'assignees',
-                                        project_meta: 'all',
-                                        orderby: 'id:desc'
-                                        
-                                    },
-                                    callback () {
-                                        if(!self.$store.state.projects.length) {
-                                            self.$router.push({
-                                                name: 'project_lists'
-                                            })
-                                        }
-                                    }
-                                });
-                                break;
-
-                            case 'incomplete':
-                                
-                                self.getProjects({
-                                    conditions:{
-                                        status: 'complete',
-                                        with: 'assignees',
-                                        project_meta: 'all',
-                                        orderby: 'id:desc'
-                                            
-                                        },
-                                    callback () {
-                                        if(!self.$store.state.projects.length) {
-                                            self.$router.push({
-                                                name: 'completed_projects'
-                                            })
-                                        }
-                                    }
-                                });
-                                break;
-
-                            default:
-                                self.getProjects({
-
-                                   with: 'assignees',
-                                   project_meta: 'all',
-                                   orderby: 'id:desc'
-                                    
-                                });
-                                break;
-                        }
-                    }
-                }
-
-                this.updateProject(args);
-            }
-        }
+export default {
+  mixins: [Mixins],
+  data() {
+    return {
+      is_active_settings: false,
+      is_update: false,
+      project: {},
+      project_action: __("Project Actions", "wedevs-project-manager"),
+      dropdownToggleStatus: false,
+      tooltipLabels: {
+        tasks: __(" Tasks", "wedevs-project-manager"),
+        discussions: __(" Discussions", "wedevs-project-manager"),
+        tasksLists: __(" Task Lists", "wedevs-project-manager"),
+        files: __(" Files", "wedevs-project-manager"),
+        milestones: __(" Milestones", "wedevs-project-manager"),
+        comments: __(" Comments", "wedevs-project-manager")
+      }
+    };
+  },
+  computed: {
+    projects() {
+      this.$root.$store.state.projects.forEach(function(project) {
+        pm.Vue.set(project, "showMoreUser", false);
+        pm.Vue.set(project, "showDropDownMenu", false);
+      });
+      return this.$root.$store.state.projects;
+    },
+    meta() {
+      return this.$root.$store.state.projects_meta;
     }
+  },
 
+  components: {
+    Favourite
+  },
+
+  created() {
+    window.addEventListener("click", this.windowActivity);
+  },
+
+  methods: {
+    windowActivity(el) {
+      var element = jQuery(el.target),
+        moreUser = element.closest(".pm-more-users"),
+        projectId = element.data("project_id"),
+        moreMenu = element.closest(".pm-has-dropdown"),
+        menuProjectId = element.data("project_id");
+
+      if (!moreUser.length) {
+        this.projects.forEach(function(project) {
+          project.showMoreUser = false;
+        });
+      } else {
+        this.projects.forEach(function(project) {
+          if (project.id != projectId) {
+            project.showMoreUser = false;
+          }
+        });
+      }
+
+      if (!moreMenu.length) {
+        this.projects.forEach(function(project) {
+          project.showDropDownMenu = false;
+        });
+      } else {
+        this.projects.forEach(function(project) {
+          if (project.id != menuProjectId) {
+            project.showDropDownMenu = false;
+          }
+        });
+      }
+    },
+    showMoreUser(project) {
+      project.showMoreUser = project.showMoreUser ? false : true;
+    },
+
+    pmDropDownOpen(project) {
+      if (project.showMoreUser) {
+        return "pm-dropdown-open";
+      }
+
+      return "";
+    },
+
+    // dropdown trigger
+    dropdownTrigger(project) {
+      project.showDropDownMenu = project.showDropDownMenu ? false : true;
+    },
+
+    // dropdown class toggler
+    dropdownToggleClass(project) {
+      if (project.showDropDownMenu) {
+        return "pm-settings pm-dropdown-menu pm-dropdown-open";
+      } else {
+        return "pm-settings pm-dropdown-menu";
+      }
+    },
+
+    settingsShowHide(project) {
+      project.settings_hide = project.settings_hide ? false : true;
+    },
+
+    projectCompleteStatus(project) {
+      var progress = 0;
+      if (typeof project.meta !== "undefined") {
+        var total_task = parseInt(project.meta.data.total_tasks, 10) || 0;
+        var completed =
+          parseInt(project.meta.data.total_complete_tasks, 10) || 0;
+        progress = (100 * completed) / total_task;
+      }
+      return {
+        width: progress + "%"
+      };
+    },
+    projectMarkAsDoneUndone(project) {
+      var self = this;
+      var status = "";
+
+      if (project.status == "complete" || project.status == "1") {
+        status = "incomplete";
+      }
+
+      if (project.status == "incomplete" || project.status == "0") {
+        status = "complete";
+      }
+
+      var args = {
+        data: {
+          id: project.id,
+          title: project.title,
+          status: status
+        },
+        callback: function(project) {
+          switch (project.data.status) {
+            case "complete":
+              self.getProjects({
+                conditions: {
+                  status: "incomplete",
+                  with: "assignees",
+                  project_meta: "all",
+                  orderby: "id:desc"
+                },
+                callback() {
+                  if (!self.$store.state.projects.length) {
+                    self.$router.push({
+                      name: "project_lists"
+                    });
+                  }
+                }
+              });
+              break;
+
+            case "incomplete":
+              self.getProjects({
+                conditions: {
+                  status: "complete",
+                  with: "assignees",
+                  project_meta: "all",
+                  orderby: "id:desc"
+                },
+                callback() {
+                  if (!self.$store.state.projects.length) {
+                    self.$router.push({
+                      name: "completed_projects"
+                    });
+                  }
+                }
+              });
+              break;
+
+            default:
+              self.getProjects({
+                with: "assignees",
+                project_meta: "all",
+                orderby: "id:desc"
+              });
+              break;
+          }
+        }
+      };
+
+      this.updateProject(args);
+    }
+  }
+};
 </script>
 
 <style lang="less">
-    .pm-project-item {
-        .dashicons-trash {
-            &:before {
-                font-size: 16px;
-                color: #eb5c70;
-            }
-        }
+.pm-project-item {
+  .dashicons-trash {
+    &:before {
+      font-size: 16px;
+      color: #eb5c70;
     }
+  }
+}
 
-    .fa-circle {
-        margin-right: 6%;
-    }
-    .no-projects {
-        margin: 0 15px 20px;
-    }
-    .pm-project-meta-counters li a[href="#"] {
-        cursor: default;
-    }
-    .pm-meta-overview i {
-        vertical-align: baseline !important;
-    }
-    .pm-meta-overview a:hover i:before {
-        color: #fff;
-    }
+.fa-circle {
+  margin-right: 6%;
+}
+.no-projects {
+  margin: 0 15px 20px;
+}
+.pm-project-meta-counters li a[href="#"] {
+  cursor: default;
+}
+.pm-meta-overview i {
+  vertical-align: baseline !important;
+}
+.pm-meta-overview a:hover i:before {
+  color: #fff;
+}
 </style>
-
-
-
