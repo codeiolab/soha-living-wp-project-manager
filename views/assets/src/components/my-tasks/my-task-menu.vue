550b94ac4de97bc0a8f400d36495ed25aafaa88b
Md Aminur Islam <eng.aminurislam@gmail.com>
2023-11-28 12:53:57 +0600

    My task panel menu active border color issue solved

--- views/assets/src/components/my-tasks/my-task-menu.vue
+++ views/assets/src/components/my-tasks/my-task-menu.vue
@@ -3,7 +3,7 @@
         <div class="pm-project-group">
             <ul class="pm-my-task-menu">
                     <li>
-                    <router-link :to="routeLink('current')" class="pm-my-outstandigntask">
+                    <router-link :to="routeLink('current')" class="pm-my-outstandigntask" :class="$route.name=='mytask-current' ? 'active' : ''">
                         {{ __('Tasks', 'wedevs-project-manager' ) }}
 
                     </router-link>
@@ -11,14 +11,14 @@
                 </li>
 
                 <li>
-                    <router-link :to="routeLink('tasks')" class="pm-my-taskoverview">
+                    <router-link :to="routeLink('tasks')" class="pm-my-taskoverview" :class="$route.name=='mytask-tasks' ? 'active' : ''">
                         {{ __('Overview', 'wedevs-project-manager' )}}
 
                     </router-link>
 
                 </li>
                 <li>
-                    <router-link :to="routeLink('activities')" class="pm-my-completetask">
+                    <router-link :to="routeLink('activities')" class="pm-my-completetask" :class="$route.name=='mytask-activities' ? 'active' : ''">
                         {{ __('Activities', 'wedevs-project-manager' )}}
 
                     </router-link>
